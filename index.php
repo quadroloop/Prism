f9ed3770f8e794b18d50f97f6e068794f4342d28
quadroloop <nplixel@gmail.com>
2018-03-23 23:53:12 +0300

    Added all UI files

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <link rel="stylesheet" href="./css/w3.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.11.0/sweetalert2.all.min.js"></script>
+  <link rel="stylesheet" href="./css/ps.css">
+    <link rel="stylesheet" href="./css/main.css">
+
+  <link href="https://fonts.googleapis.com/css?family=Hind" rel="stylesheet">
+  <title>Prism</title>
+  <style>
+    h4 {
+      font-family: 'Hind', sans-serif;
+    }
+  </style>
+</head>
+<body class="w3-white">
+
+<div class="w3-sidebar w3-bar-block w3-card-4 w3-animate-left" style="display:none;overflow:auto;position: relative;" id="sidebar">
+  <a class="w3-bar-item w3-large" onclick="close_sidebar();">
+    <center>
+       <img src="./img/prism.png" style="width:120px;">
+       <hr class="w3-card-2">
+    </center>
+  </a>
+   <a id="tool_state" class="dark-border w3-round w3-padding-small w3-text-grey tool_state"><i class="fa fa-map w3-text-grey"></i> MENU</a>
+  <a onclick="call_editor();" class="w3-bar-item w3-button w3-hover-pale-green"><i class="fa fa-pencil w3-text-blue"></i> Editor</a>
+  <a onclick="getRoute();" class="w3-bar-item w3-button w3-hover-pale-green"><i class="fa fa-sitemap w3-text-green"></i> Get Route Link</a>
+  <a onclick="viewroute();" class="w3-bar-item w3-button w3-hover-pale-green"><i class="fa fa-eye w3-text-amber"></i> View Route</a>
+  <a onclick="restart();" class="w3-bar-item w3-button w3-hover-pale-green"><i class="fa fa-refresh w3-text-pink"></i> Reset Data</a>
+  <a onclick="about_app();" class="w3-bar-item w3-button w3-hover-pale-green"><i class="fa fa-file-text-o w3-text-purple"></i> About</a>
+  <a href="https://github.com/quadroloop/Prism" class="w3-bar-item w3-button w3-hover-pale-green"><i class="fa fa-github w3-text-black"></i> View on Github!</a>
+  <br>
+  <br>
+   <a id="tool_state" class="dark-border w3-round w3-padding-small w3-text-grey tool_state"><i class="fa fa-cog w3-text-grey"></i> SETTINGS</a>
+  <a href="call_editor();" class="w3-bar-item w3-button w3-hover-pale-green"><i class="fa fa-circle w3-text-amber"></i> Javascript</a>
+  <a href="getRoute();" class="w3-bar-item w3-button w3-hover-pale-green"><i class="fa fa-circle w3-text-orange"></i> JSON</a>
+  <a oclick="viewroute();" class="w3-bar-item w3-button w3-hover-pale-green"><i class="fa fa-circle w3-text-indigo"></i> PHP</a>
+  <a href="editor();" class="w3-bar-item w3-button w3-hover-pale-green"><i class="fa fa-circle w3-text-pink"></i> HTML</a>
+  <a href="getRoute();" class="w3-bar-item w3-button w3-hover-pale-green"><i class="fa fa-circle w3-text-purple"></i> CSS</a>
+</div>
+
+
+    <div class="w3-bar w3-white">
+      <a onclick="open_sidebar();">
+      <h4 class="w3-bar-item w3-padding-small w3-text-teal">    
+      <img src="./img/prism.png" style="width:120px;">
+      </a>
+    </h4>
+      <a href="https://github.com/quadroloop/Prism" title="View on Github!"><i class="fa fa-github w3-text-grey fa-3x w3-right w3-bar-item w3-button w3-round"></i></a>
+    </div> 
+<div id="main">  
+  <center>
+   <ul class="w3-ul w3-margin" id="samples">
+  <?php
+  $fileList = glob('./Three.js/examples/*.html');
+foreach($fileList as $filename){
+    echo '
+    <li class="w3-bar w3-hover-pale-blue" style="cursor:pointer" title="'.$filename.'" onclick="view(this.title)">
+    <i  class="w3-bar-item fa fa-circle w3-text-light-green w3-margin w3-white w3-xlarge w3-right"></i>
+     <i class="w3-bar-item fa fa-bars w3-margin w3-text-purple w3-white w3-xlarge w3-right"></i>
+      <img src="./img/'.rand(1,11).'.png" class="w3-bar-item w3-circle w3-hide-small" style="width:85px">
+      <div class="w3-bar-item">
+        <span class="w3-large w3-text-indigo">'.$filename.'</span><br>
+        <span class="w3-text-grey">Three.js Animation File</span>
+      </div>
+    </li>
+
+    ';
+}
+?>
+</ul>
+      <iframe src="" style="width:100vw;height:100vh;display: none;border: 0px;" id="viewer"></iframe>
+      <div id="editor" class="w3-card-4 w3-hide"></div>
+        <div id="about">
+          <h1>this is what it's about</h1>
+        </div>  
+      <br>   
+      <br>
+<!--     <button class="w3-text-white w3-btn w3-round w3-teal" onclick="deploy();"><i class="fa fa-send-o"></i> Create Response !</button>
+ -->  </center>
+ </div> 
+
+
+
+
+
+<script type="text/javascript" src="./js/perfect-scrollbar.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.3.1/ace.js"></script>
+<script src="./js/main.js"></script>
+</body>
+</html>
+
---
+++ js/main.js
@@ -0,0 +1,85 @@
+// main Javascript file for Octolroute App
+// Author Bryce C. Mercines 2018
+
+//init sidebar-scroll
+
+var userToken;
+
+var ps = new PerfectScrollbar('#sidebar');
+
+window.onload = function(){
+
+  var editorscroll = new PerfectScrollbar('.ace_scrollbar');
+  var editorscroll_h = new PerfectScrollbar('.ace_scrollbar-h');
+
+
+  // set user Token
+  userToken = generate_token(15);
+}
+
+//sidebar UI
+function open_sidebar() {
+  document.getElementById("main").style.marginLeft = "15%";
+  document.getElementById("sidebar").style.width = "15%";
+  document.getElementById("sidebar").style.display = "block";
+}
+function close_sidebar() {
+  document.getElementById("main").style.marginLeft = "0%";
+  document.getElementById("sidebar").style.display = "none";
+}
+
+
+// init editor
+
+ var editor = ace.edit("editor");
+    editor.setTheme("ace/theme/monokai");
+    editor.session.setMode("ace/mode/javascript");
+        editor.setAutoScrollEditorIntoView(true);
+
+
+
+// functions
+
+
+   function deploy() {
+   	  if(editor.getValue() != [ ]){
+   	  	swal("Success!","you have successfully deployed","success");
+   	  }
+   }
+
+
+   // editor mode..
+    function call_editor() {
+    	var img = document.getElementById('logo').style.display = "none";
+    	var coder = document.getElementById('editor');
+    	coder.style = "border-radius:10px;width:850px;height:450px;";
+    	coder.class = "w3-animate-opacity";
+        editor.setTheme("ace/theme/ambiance");
+    editor.session.setMode("ace/mode/HTML");
+    }
+
+   //about
+
+   function about_app() {
+   	  document.getElementById("editor").style.display = "none";
+   	  document.getElementById("about").style.display = "block";
+   }
+//generate userToken
+ function generate_token(length){
+    //edit the token allowed characters
+    var a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".split("");
+    var b = [];  
+    for (var i=0; i<length; i++) {
+        var j = (Math.random() * (a.length-1)).toFixed(0);
+        b[i] = a[j];
+    }
+    return b.join("");
+}
+
+function view(url) {
+  var x = document.getElementById('viewer');
+  var y = document.getElementById('samples');
+  y.style.display = 'none';
+  x.style.display = 'block';
+  x.src = url;
+}
